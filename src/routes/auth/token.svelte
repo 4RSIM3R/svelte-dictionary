d853dfe749b505c45632d51e8f63902cd857db34
ilzam <ilzammulkhaq85@gmail.com>
2022-08-15 07:56:44 +0700

    feat: adding get token

---
+++ src/routes/auth/token.svelte
@@ -0,0 +1,30 @@
+<script>
+    import {
+        getAuth,
+        GoogleAuthProvider,
+        signInWithPopup,
+    } from "firebase/auth";
+
+    import { app } from "../../utils/auth_utils";
+
+    let token = "";
+
+    const loginWithGoogle = () => {
+        const auth = getAuth(app);
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider).then((value) => {
+            token = value.user.accessToken;
+        });
+    };
+
+    const copyToken = () => {
+        navigator.clipboard.writeText(token);
+    };
+</script>
+
+<div
+    class="min-h-screen max-w-sm flex flex-col justify-center items-center space-y-8"
+>
+    <button on:click={loginWithGoogle}> Get Token Here </button>
+    <button on:click={copyToken}> Copy Token </button>
+</div>
